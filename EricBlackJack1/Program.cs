b14cceedd22ff40510f51529dbb8fdf38e460aae
Meysam03 <alre03001@utb.vaxjo.se>
2022-12-09 11:12:52 +0100

    Add project files.

---
+++ EricBlackJack1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EricBlackJack1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej och välkommen till BlackJack")
+        }
+    }
+}
